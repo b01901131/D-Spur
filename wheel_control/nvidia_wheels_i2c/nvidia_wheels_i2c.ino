5c5750946e355114829521a8175e944064ae0cbe
Tzu-Ray Su <b01901007@ntu.edu.tw>
2016-09-17 15:55:22 +0800

    Update wheel .ino files.

--- wheel_control/nvidia_wheels_i2c/nvidia_wheels_i2c.ino
+++ wheel_control/nvidia_wheels_i2c/nvidia_wheels_i2c.ino
@@ -1,8 +1,5 @@
-
+#include <compat/twi.h>
 #define BAUD 38400
-//#define C 0.000002
-// 100 rad/s*s
-//#define P 31415.926535 / 16
 
 /*
  * PORTA: 29    28    27    26    25    24    23    22
@@ -14,35 +11,32 @@
  * PORTL: 42    43    44    45    46    47    48    49
  *        DIR3   [     MS3    ]    DIR4   [     MS4    ]
  *
- * TIMER3: 2    3     5
+ * TIMER1 TIMER3 TIMER4 
  *
+ * I2C: 20 21
  *
  */
+unsigned char SLA = 4;
+
 int i;
 double C = 0.000002;
 double P = 31415.926535 / 16;
 
 byte rcv_count;
-byte rcv_buffer[10];
+byte rcv_buffer[18];
+volatile bool i2c_done = false;
 
 volatile unsigned long timer_count[4] = {0, 0, 0, 0};
 volatile unsigned long step_count[4] = {0, 0, 0, 0}; 
 int mod = 1;
-int wait_us[4] = {2000, 2000, 2000, 2000};
-//int wait_us[4] = {1000, 1000, 1000, 1000};
-double precise_wait_us[4] = {2000.0, 2000.0, 2000.0, 2000.0};
-//int wait_us[4] = {2000, 2000, 2000, 2000};
+int wait_us[4] = {1000, 1000, 1000, 1000};
+double precise_wait_us[4] = {1000.0, 1000.0, 1000.0, 1000.0};
 int target_wait_us[4] = {-1, -1, -1, -1};
 //int target_wait_us[4] = {1000, 1000, 1000, 1000};
-//int target_wait_us[4] = {2000, 2000, 2000, 2000};
-//int target_wait_us[4] = {500, 500, 500, 500};
-//bool accelerating[4] = {true, true, true, true};
-bool accelerating[4] = {false, false, false, false};
+bool accelerating[4] = {true, true, true, true};
 
 byte dir_upper = B01111111;  // set DIR1=LOW MS1 DIR2=HIGH MS2 
 byte dir_lower = B01111111;  // set DIR3=LOW MS3 DIR4=HIGH MS4 
-//byte dir_upper = B01011101;  // set DIR1=LOW MS1 DIR2=HIGH MS2 
-//byte dir_lower = B01011101;  // set DIR3=LOW MS3 DIR4=HIGH MS4
 volatile byte step_byte = B00000000;
 
 union u_tag {
@@ -52,18 +46,9 @@ union u_tag {
 
 void setup() 
 {
-  //Serial.begin(9600);
-  //Serial3.begin(BT_BAUD);
-  unsigned int baud_setting = (F_CPU / 16 / BAUD) - 1;
-  // Set the baud rate
-  UBRR0H = (unsigned char)(baud_setting >> 8);
-  UBRR0L = (unsigned char)baud_setting;
-  // Enable receiver and transmitter, no interrupt
-  UCSR0B |= _BV(RXEN0) | _BV(TXEN0);
-  UCSR0B |= _BV(RXCIE0);
-  UCSR0B &= ~_BV(TXCIE0);   
-  // Set frame format: No parity check, 8 Data bits, 1 stop bit
-  UCSR0C |= _BV(UCSZ01) | _BV(UCSZ00);
+  Serial.begin(9600);
+  Wire.begin(4);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
   
   // set pin directions to OUTPUT.
   DDRA = B11111111;
@@ -119,6 +104,51 @@ void setup()
 
 void loop() 
 { 
+  if(i2c_done){
+    noInterrupts();
+    for (i = 0; i< 4; i++){
+      u.b[3] = rcv_buffer[0+4*i];
+      u.b[2] = rcv_buffer[1+4*i];
+      u.b[1] = rcv_buffer[2+4*i];
+      u.b[0] = rcv_buffer[3+4*i];
+      target_wait_us[i] = u.ival;
+      if(target_wait_us[i] != -1){
+        accelerating[i] = (target_wait_us[i] < wait_us[i]);
+      }
+    }
+    // check gear shifting
+    if (target_wait_us[0] != -1 &&
+        (dir_upper & 0x70) != (rcv_buffer[16] & 0x70))
+      wait_us[0] = target_wait_us[0];
+    if (target_wait_us[1] != -1 &&
+        (dir_upper & 0x07) != (rcv_buffer[16] & 0x07))
+      wait_us[1] = target_wait_us[1];
+    if (target_wait_us[2] != -1 &&
+        (dir_lower & 0x70) != (rcv_buffer[17] & 0x70))
+      wait_us[2] = target_wait_us[2];
+    if (target_wait_us[3] != -1 &&
+        (dir_lower & 0x07) != (rcv_buffer[17] & 0x07))
+      wait_us[3] = target_wait_us[3];
+    
+    dir_upper = rcv_buffer[16];
+    dir_lower = rcv_buffer[17];
+    
+    // set output pin port register for DIRx & MSx
+    PORTC = dir_upper;
+    PORTL = dir_lower;
+    
+    rcv_count = 0;
+    i2c_done = false;
+    interrupts();
+    Serial.print("target: ");
+    Serial.print(wait_us[0]);
+    Serial.print(" ");
+    Serial.print(wait_us[1]);
+    Serial.print(" ");
+    Serial.print(wait_us[2]);
+    Serial.print(" ");
+    Serial.print(wait_us[3]);
+  }
 }
 
 // Interrupt Service Routine for motor control.
@@ -156,7 +186,7 @@ ISR (TIMER1_OVF_vect) {
       }    
     } else {
       // stand-by. set wait_us init val.
-      wait_us[0] = 2000;
+      wait_us[0] = 1000;
     }
   }
   
@@ -200,7 +230,7 @@ ISR (TIMER3_OVF_vect) {
       }    
     } else {
       // stand-by. set wait_us init val.
-      wait_us[1] = 2000;
+      wait_us[1] = 1000;
     }
   }
   
@@ -244,7 +274,7 @@ ISR (TIMER4_OVF_vect) {
       }    
     } else {
       // stand-by. set wait_us init val.
-      wait_us[2] = 2000;
+      wait_us[2] = 1000;
     }
   }
   
@@ -287,7 +317,7 @@ ISR (TIMER5_OVF_vect) {
       }    
     } else {
       // stand-by. set wait_us init val.
-      wait_us[3] = 2000;
+      wait_us[3] = 1000;
     }
   }
   // reset timer
@@ -296,27 +326,25 @@ ISR (TIMER5_OVF_vect) {
 
 // Interrrupt Service Routine for receiving data from Serial
 /*
- *  protocol : 18 bytes
- *    (int)  target_wait_us[0] : 4 bytes
- *    (int)  target_wait_us[1] : 4 bytes
- *    (int)  target_wait_us[2] : 4 bytes
- *    (int)  target_wait_us[3] : 4 bytes
+ *  protocol : 10 bytes
+ *    (int)  target_wait_us[0] : 2 bytes
+ *    (int)  target_wait_us[1] : 2 bytes
+ *    (int)  target_wait_us[2] : 2 bytes
+ *    (int)  target_wait_us[3] : 2 bytes
  *    (byte) dir_upper      : 1 byte
  *    (byte) dir_lower      : 1 byte
  */
-ISR (USART0_RX_vect){
+/*ISR (USART0_RX_vect){
   rcv_buffer[rcv_count] = UDR0;
   rcv_count++;
   
   // transmission complete
-  if(rcv_count == 10){
+  if(rcv_count == 18){
     for (i = 0; i< 4; i++){
-      //u.b[3] = rcv_buffer[0+4*i];
-      //u.b[2] = rcv_buffer[1+4*i];
-      //u.b[1] = rcv_buffer[2+4*i];
-      //u.b[0] = rcv_buffer[3+4*i];
-      u.b[1] = rcv_buffer[0+2*i];
-      u.b[0] = rcv_buffer[1+2*i];
+      u.b[3] = rcv_buffer[0+4*i];
+      u.b[2] = rcv_buffer[1+4*i];
+      u.b[1] = rcv_buffer[2+4*i];
+      u.b[0] = rcv_buffer[3+4*i];
       target_wait_us[i] = u.ival;
       if(target_wait_us[i] != -1){
         accelerating[i] = (target_wait_us[i] < wait_us[i]);
@@ -324,20 +352,20 @@ ISR (USART0_RX_vect){
     }
     // check gear shifting
     if (target_wait_us[0] != -1 &&
-        (dir_upper & 0x70) != (rcv_buffer[8] & 0x70))
+        (dir_upper & 0x70) != (rcv_buffer[16] & 0x70))
       wait_us[0] = target_wait_us[0];
     if (target_wait_us[1] != -1 &&
-        (dir_upper & 0x07) != (rcv_buffer[8] & 0x07))
+        (dir_upper & 0x07) != (rcv_buffer[16] & 0x07))
       wait_us[1] = target_wait_us[1];
     if (target_wait_us[2] != -1 &&
-        (dir_lower & 0x70) != (rcv_buffer[9] & 0x70))
+        (dir_lower & 0x70) != (rcv_buffer[17] & 0x70))
       wait_us[2] = target_wait_us[2];
     if (target_wait_us[3] != -1 &&
-        (dir_lower & 0x07) != (rcv_buffer[9] & 0x07))
+        (dir_lower & 0x07) != (rcv_buffer[17] & 0x07))
       wait_us[3] = target_wait_us[3];
     
-    dir_upper = rcv_buffer[8];
-    dir_lower = rcv_buffer[9];
+    dir_upper = rcv_buffer[16];
+    dir_lower = rcv_buffer[17];
     
     // set output pin port register for DIRx & MSx
     PORTC = dir_upper;
@@ -346,5 +374,46 @@ ISR (USART0_RX_vect){
     rcv_count = 0;
   }
   
-}
+}*/
+/*
+ISR(TWI_vect) {		// TWI interrupt service routine
+  switch(TW_STATUS){
+    case TW_SR_SLA_ACK:
+      // 2) receive byte
+      TWCR = (1 << TWEA) | (1 << TWEN) | (1<< TWINT) | (1<<TWIE);
+      break;
+    
+    case TW_SR_DATA_ACK:
+      rcv_buffer[rcv_count] = TWDR;
+      rcv_count++;
+          
+      // transmission complete
+      if(rcv_count == 18){
+        i2c_done = true;
+      }
 
+      // 3) stop receiving: send nack for next receiving bytes
+      TWCR = (1 << TWEN) | (1<< TWINT) | (1<<TWIE);
+      break;
+    
+    case TW_SR_STOP:
+      // 4) release bus
+      TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT) | (1<<TWIE);
+      break;
+    
+    case TW_ST_SLA_ACK:
+      // 5) send  byte
+      TWDR = 6;
+      TWCR = (1 << TWEN) | (1 << TWINT) | (1<<TWIE);
+      break;
+    
+    case TW_ST_DATA_NACK:
+      // 6) release bus
+      TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT) | (1<<TWIE); 
+      break;
+    
+    default:
+      // 9) check for errors and if one occurs send stop condition
+      TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWIE)| (1 << TWEA);
+  }
+}*/
