2ab738f35a1d0aeaa97417c46163cf1128bb22ff
Tzu-Ray Su <b01901007@ntu.edu.tw>
2016-09-17 15:12:51 +0800

    test code for serial WheelController.

--- WheelController/test_serialAPI_4timer.cpp
+++ WheelController/test_serialAPI_4timer.cpp
@@ -11,36 +11,21 @@
 using namespace std;
 
 int main (void){
-  //SerialPortController* serial_port_controller = new SerialPortController("/dev/cu.wchusbserial1410");
-  SerialPortController* serial_port_controller = NULL;
+  SerialPortController* serial_port_controller = new SerialPortController("/dev/cu.wchusbserial1410");
+  //SerialPortController* serial_port_controller = NULL;
 
   WheelController* wheel = new WheelController(serial_port_controller);
 
-  //uint8_t wheel_protocol[22];
   char cmd[20];
   double vx = 0;
   double vy = 0;
   double w0 = 0;
-
-  //double w1,w2,w3,w4;
-  //int wait_us1,wait_us2,wait_us3,wait_us4;
-  //int step_mode = 16;
 
   while (1) {
     scanf("%s %lf %lf %lf", cmd, &vx, &vy, &w0);
     if (cmd[0] == 'q')
       break;
     else if(cmd[0] == 's'){
-      /*
-      int stop = -1;
-
-      wheel_protocol[0] = (stop >> 24) & 0xff;
-      wheel_protocol[1] = (stop >> 16) & 0xff;
-      wheel_protocol[2] = (stop >> 8) & 0xff;
-      wheel_protocol[3] = (stop & 0xff);
-
-      serial_port_controller->sendByte(wheel_protocol, 18);
-      */
       wheel->stopMotion();
       continue;
     }
